8a121a3df67b926f194ed5255870e5742ad1ecd6
Andresp08 <Andresfp29032001@gmail.com>
2022-11-04 09:45:30 -0500

    finish app development

---
+++ src/main/java/com/co/andresoft/app/SpringBootDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.co.andresoft.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDataJpaApplication.class, args);
+	}
+
+}
